42381d23da3f1431cfd9b4c4416b4a6f4dde1e7e
lazom <lazom.dzemaili@gmail.com>
2018-12-08 17:58:25 +0100

    initialisation du projet

---
+++ src/main/java/com/dzemaili/gurutraining/GurutrainingApplication.java
@@ -0,0 +1,12 @@
+package com.dzemaili.gurutraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurutrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurutrainingApplication.class, args);
+	}
+}
